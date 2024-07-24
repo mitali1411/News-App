4e11d7718360a39953e49999eec2fa16bd1a93ed
Mittali Rasaniya <148072662+mitali1411@users.noreply.github.com>
2024-07-24 21:55:23 +0530

    Update NewsAction.jsx

--- src/context/theme/news/NewsAction.jsx
+++ src/context/theme/news/NewsAction.jsx
@@ -1,5 +1,5 @@
 export const fetchNews = async (topic) => {
-    const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2024-04-20&sortBy=publishedAt&apiKey=0057d2481f7b4ec9ac308036197cf8cf`);
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2024-05-15&sortBy=publishedAt&apiKey=0057d2481f7b4ec9ac308036197cf8cf`);
     const data = await response.json();
     return data.articles;
 }
